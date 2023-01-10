589393d45108346d4bf89e91c3a39b326bd457da
idimitrrov <idimitrrov@gmail.com>
2023-01-10 10:22:39 +0100

    Finished the first project and uploading it

--- 01-Project/app.js
+++ 01-Project/app.js
@@ -5,41 +5,45 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-// Load all event listeners
+// Load all event listeners 
 loadEventListeners();
 
-// Load all event listeners
-function loadEventListeners() {
+// Load all event listeners 
+function loadEventListeners(){
   // Add task event
   form.addEventListener('submit', addTask);
 }
 
 // Add Task
+
 function addTask(e) {
-  if(taskInput.value === '') {
+  if(taskInput.value === ''){
     alert('Add a task');
   }
-
+  
   // Create li element
+
   const li = document.createElement('li');
   // Add class
   li.className = 'collection-item';
   // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
-  const link = document.createElement('a');
+  const link = document.createElement ('a');
   // Add class
   link.className = 'delete-item secondary-content';
   // Add icon html
   link.innerHTML = '<i class="fa fa-remove"></i>';
   // Append the link to li
+
   li.appendChild(link);
 
   // Append li to ul
+
   taskList.appendChild(li);
 
   // Clear input
-  taskInput.value = '';
-
+  taskInput.value = '';  
+  console.log(li);
   e.preventDefault();
 }
