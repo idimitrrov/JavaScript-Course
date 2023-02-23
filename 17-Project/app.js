241d93d8bdc081962150b2fdbea0285622e8dad4
idimitrrov <idimitrrov@gmail.com>
2023-02-23 16:18:34 +0100

    Updated the code, so that it works with the newer API from Chuck Norris

--- 17-Project/app.js
+++ 17-Project/app.js
@@ -3,27 +3,32 @@ document.querySelector(".get-jokes").addEventListener("click", getJokes);
 function getJokes(e) {
   const number = document.querySelector('input[type="number"]').value;
 
-  const xhr = new XMLHttpRequest();
+  // Clear existing jokes
+  document.querySelector(".jokes").innerHTML = "";
 
-  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+  for (let i = 0; i < number; i++) {
+    const xhr = new XMLHttpRequest();
 
-  xhr.onload = function () {
-    if (this.status === 200) {
-      const response = JSON.parse(this.responseText);
+    xhr.open("GET", "https://api.chucknorris.io/jokes/random", true);
 
-      let output = "";
+    xhr.onload = function () {
+      if (this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        console.log(response);
+        let output = "";
 
-      if (response.type === "success") {
-        response.value.forEach(function (joke) {
-          output += `<li>${joke.joke}</li>`;
-        });
-      } else {
-        output += "<li>Something went wrong</li>";
+        if (response.type === "success") {
+          output += "";
+        } else {
+          output += `<li>${response.value}</li>`;
+        }
+
+        document.querySelector(".jokes").innerHTML += output;
       }
+    };
 
-      document.querySelector(".jokes").innerHTML = output;
-    }
-  };
+    xhr.send();
+  }
 
   e.preventDefault();
 }
