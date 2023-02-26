8ac828bbf42d80c712ad5852f24aea3ee9df8da4
idimitrrov <idimitrrov@gmail.com>
2023-02-26 02:52:22 +0100

    Creating AJAX Library

--- 19-Project/app.js
+++ 19-Project/app.js
@@ -1,57 +1,14 @@
-const posts = [
-  { title: "Post One", body: "This is post one" },
-  { title: "Post Two", body: "This is post two" },
-];
+const http = new easyHTTP();
 
-// function createPost() {
-//   setTimeout(() => {
-//     posts.push(post);
-//   }, 2000);
-// }
+// Get Posts
 
-// const createPost = () => {
-//   setTimeout(() => {
-//     posts.push(post);
-//   }, 2000);
-// };
-
-// function getPosts() {
-//   setTimeout(() => {
-//     let output = "";
-//     posts.forEach((post) => {
-//       output += `<li>${post.title}</li>`;
-//     });
-//     document.body.innerHTML = output;
-//   }, 1000);
-// }
-
-// const getPosts = () => {
-//   setTimeout(() => {
-//     let output = "";
-//     posts.forEach((post) => {
-//       output += `<li>${post.title}</li>`;
-//     });
-//     document.body.innerHTML = output;
-//   }, 1000);
-// };
-
-const createPost = (post, callback) => {
-  setTimeout(() => {
-    posts.push(post);
-    callback();
-  }, 2000);
-};
-
-const getPosts = () => {
-  setTimeout(() => {
-    let output = "";
-    posts.forEach((post) => {
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000);
-};
-
-createPost({ title: "Post Three", body: "This is post three" }, getPosts);
-
-getPosts();
+const posts = http.get(
+  "https://jsonplaceholder.typicode.com/posts",
+  (err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(posts);
+    }
+  }
+);
---
+++ 19-Project/easyhttp.js
@@ -0,0 +1,43 @@
+function easyHTTP() {
+  this.http = new XMLHttpRequest();
+}
+
+// // Make an HTTP GET Request
+
+easyHTTP.prototype.get = function (url, callback) {
+  this.http.open("GET", url, true);
+
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback("Error: " + self.http.status);
+    }
+  };
+
+  this.http.send();
+};
+
+// Testing the above code with class
+
+// class easyHTTP {
+//     constructor() {
+//     this.http = new XMLHttpRequest();
+//   }
+//   get(url) {
+//     this.http.open("GET", url, true);
+//     this.http.onload = () => {
+//       if (this.http.status === 200) {
+//         console.log(this.http.responseText);
+//       }
+//     };
+//     this.http.send();
+//   }
+// }
+
+// Make an HTTP POST Request
+
+// Make an HTTP PUT Request
+
+// Make an HTTP DELETE Request
