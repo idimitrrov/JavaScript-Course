44cc59629ed92cad6f6ee1b23e97722cc83189a6
idimitrrov <idimitrrov@gmail.com>
2023-03-01 21:27:41 +0100

    Editted the code, to have authorisation, removed the authorisation token

--- 26-Project-GithubFinder/github.js
+++ 26-Project-GithubFinder/github.js
@@ -1,18 +1,28 @@
 class GitHub {
   constructor() {
-    this.client_id = "4ce6b4032674035eca45";
-    this.client_secret = "9544a424c59a976b3b74adf0a74d96833151b1c6";
     this.repos_count = 5;
     this.repos_sort = "created: asc";
+    this.requestInit = {
+      method: "GET",
+      headers: new Headers({
+        // "Access-Control-Allow-Origin": "*",
+        Accept: "application/vnd.github+json",
+        Authorization:
+          "token " +
+          "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",
+      }),
+    };
   }
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}`,
+      this.requestInit
     );
 
     const reposResponse = await fetch(
-      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort${this.repos_sort}`,
+      this.requestInit
     );
 
     const profile = await profileResponse.json();
