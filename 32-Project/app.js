bfefe0850f94d27fa9962aed260b51ab11633a4f
idimitrrov <idimitrrov@gmail.com>
2023-03-04 05:11:10 +0100

    Scroller update

--- 32-Project/app.js
+++ 32-Project/app.js
@@ -16,63 +16,56 @@ const data = [
     image: "https://randomuser.me/api/portraits/women/82.jpg",
   },
   {
-    name: "Cleopatra X",
-    age: 31,
-    gender: "female",
-    lookingfor: "male",
-    location: "Lynn MA",
-    image: "https://randomuser.me/api/portraits/women/83.jpg",
-  },
-  {
-    name: "John Douglas",
-    age: 32,
+    name: "William Johnson",
+    age: 38,
     gender: "male",
     lookingfor: "female",
-    location: "Boston MA",
-    image: "https://randomuser.me/api/portraits/men/81.jpg",
+    location: "Lynn MA",
+    image: "https://randomuser.me/api/portraits/men/83.jpg",
   },
 ];
 
 const profiles = profileIterator(data);
 
-// Next Event
+// Call first profile
+nextProfile();
 
+// Next Event
 document.getElementById("next").addEventListener("click", nextProfile);
 
 // Next Profile Display
-
 function nextProfile() {
   const currentProfile = profiles.next().value;
 
-  document.getElementById("profileDisplay").innerHTML = `
-    <ul class="list-group">
-      <li class="list-group-item"Name: ${currentProfile.name}</li>
-      <li class="list-group-item"Name: ${currentProfile.age}</li>
-      <li class="list-group-item"Name: ${currentProfile.gender}</li>
-      <li class="list-group-item"Preference: ${currentProfile.lookingfor}</li>
-      <li class="list-group-item"Name: ${currentProfile.location}</li>
-    </ul>
-  `;
+  if (currentProfile !== undefined) {
+    document.getElementById("profileDisplay").innerHTML = `
+      <ul class="list-group">
+        <li class="list-group-item">Name: ${currentProfile.name}</li>
+        <li class="list-group-item">Age: ${currentProfile.age}</li>
+        <li class="list-group-item">Location: ${currentProfile.location}</li>
+        <li class="list-group-item">Gender: ${currentProfile.gender} 
+        <li class="list-group-item">Looking for: ${currentProfile.lookingfor}</li>
+      </ul>
+    `;
 
-  document.getElementById("imageDisplay").innerHTML = `
-  <img src="${currentProfile.image}">
-`;
+    document.getElementById(
+      "imageDisplay"
+    ).innerHTML = `<img src="${currentProfile.image}">`;
+  } else {
+    // No more profiles
+    window.location.reload();
+  }
 }
-// Profile Iterator
 
+// Profile Iterator
 function profileIterator(profiles) {
   let nextIndex = 0;
 
   return {
     next: function () {
       return nextIndex < profiles.length
-        ? {
-            value: profiles[nextIndex++],
-            done: false,
-          }
-        : {
-            done: true,
-          };
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
     },
   };
 }
