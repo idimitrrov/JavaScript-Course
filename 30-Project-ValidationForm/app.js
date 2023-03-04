1d25f56204080a59c9cd5fd199956afc5cb23b68
idimitrrov <idimitrrov@gmail.com>
2023-03-04 17:42:02 +0100

    Refactored the code to look better

--- 30-Project-ValidationForm/app.js
+++ 30-Project-ValidationForm/app.js
@@ -1,108 +1,55 @@
 const validateName = () => {
   const name = document.getElementById("name");
-  const reName = /^[a-zA-Z\s]{1,20}$/;
-  const reSymbols = /[\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+  const re = /^[A-Za-z\s]*$/;
 
-  if (name.value === "") {
-    name.classList.remove("is-invalid");
-    let invalidName = () => {
-      let newValue = document.getElementById("invalid-name");
-      newValue.innerHTML = "";
-    };
-    invalidName();
-    return;
-  } else if (reSymbols.test(name.value)) {
-    name.classList.add("is-invalid");
-    let invalidName = () => {
-      let newValue = document.getElementById("invalid-name");
-      newValue.innerHTML = "Name cannot contain special characters!";
-    };
-    invalidName();
-    throw new Error("Name cannot contain special characters or numbers!");
-  } else if (!reName.test(name.value)) {
-    name.classList.add("is-invalid");
-    let invalidName = () => {
-      let newValue = document.getElementById("invalid-name");
-      newValue.innerHTML = "Please enter a name of maximum 20 characters!";
-    };
-    invalidName();
-    throw new Error("Please enter a name of maximum 20 characters!");
+  if (!re.test(name.value)) {
+    validateField("name", re, "Name cannot contain special characters!");
   } else {
-    name.classList.remove("is-invalid");
+    validateField(
+      "name",
+      /^[a-zA-Z\s]{1,20}$/,
+      "Name cannot exceed 20 characters."
+    );
   }
+  console.log(!re.test(name.value));
 };
-const validateZip = () => {
-  const zip = document.getElementById("zipcode");
-  const re = /^[0-9]{5}(-[0-9]{4})?$/;
 
-  if (zip.value === "") {
-    zip.classList.remove("is-invalid");
-    let invalidName = () => {
-      let newValue = document.getElementById("invalid-zip");
-      newValue.innerHTML = "";
-    };
-    invalidName();
-    return;
-  } else if (!re.test(zip.value)) {
-    zip.classList.add("is-invalid");
-    let invalidZip = () => {
-      let newValue = document.getElementById("invalid-zip");
-      newValue.innerHTML = "Zipcode is not valid. Enter a new one.";
-    };
-    invalidZip();
-    throw new Error("Zipcode is not valid. Enter a new one.");
+const validateField = (type, re, error) => {
+  const element = document.getElementById(type);
+  let errorTextContainer = document.getElementById(`invalid-${type}`);
+  if (element.value === "") {
+    element.classList.remove("is-invalid");
+    errorTextContainer.innerHTML = "";
+  } else if (!re.test(element.value)) {
+    element.classList.add("is-invalid");
+    errorTextContainer.innerHTML = error;
   } else {
-    zip.classList.remove("is-invalid");
-  }
-};
-const validateEmail = () => {
-  const email = document.getElementById("email");
-  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-  if (email.value === "") {
-    email.classList.remove("is-invalid");
-    let invalidEmail = () => {
-      let newValue = document.getElementById("invalid-email");
-      newValue.innerHTML = "";
-    };
-    invalidEmail();
-    return;
-  } else if (!re.test(email.value)) {
-    email.classList.add("is-invalid");
-    let invalidEmail = () => {
-      let newValue = document.getElementById("invalid-email");
-      newValue.innerHTML = "Enter a valid email";
-    };
-    invalidEmail();
-    throw new Error("Email is not valid.");
-  } else {
-    email.classList.remove("is-invalid");
-  }
-};
-const validatePhone = () => {
-  const phone = document.getElementById("phone");
-  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
-  if (phone.value === "") {
-    phone.classList.remove("is-invalid");
-    let invalidPhone = () => {
-      let newValue = document.getElementById("invalid-phone");
-      newValue.innerHTML = "";
-    };
-    invalidPhone();
-    return;
-  } else if (!re.test(phone.value)) {
-    phone.classList.add("is-invalid");
-    let invalidPhone = () => {
-      let newValue = document.getElementById("invalid-phone");
-      newValue.innerHTML = "Enter a valid phone";
-    };
-    invalidPhone();
-  } else {
-    phone.classList.remove("is-invalid");
+    element.classList.remove("is-invalid");
   }
 };
 
 // Form Blur Event Listeners
 document.getElementById("name").addEventListener("blur", validateName);
-document.getElementById("zipcode").addEventListener("blur", validateZip);
-document.getElementById("email").addEventListener("blur", validateEmail);
-document.getElementById("phone").addEventListener("blur", validatePhone);
+document
+  .getElementById("zipcode")
+  .addEventListener("blur", () =>
+    validateField("zipcode", /^[0-9]{5}(-[0-9]{4})?$/, "Enter a valid zipcode")
+  );
+document
+  .getElementById("email")
+  .addEventListener("blur", () =>
+    validateField(
+      "email",
+      /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+      "Enter a valid email"
+    )
+  );
+document
+  .getElementById("phone")
+  .addEventListener("blur", () =>
+    validateField(
+      "phone",
+      /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/,
+      "Enter a valid phone"
+    )
+  );
